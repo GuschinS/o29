d33c53f10adf6a68072cce57dfea916fc060203f
GuschinS <guschins@gmail.com>
2021-08-07 13:05:38 +0300

    edit yandex.js

--- js/yandex.js
+++ js/yandex.js
@@ -11,7 +11,7 @@
         ecommerce:"dataLayer"
    });
 
-/* <noscript><div><img src="https://mc.yandex.ru/watch/21935599" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */}
+/* <noscript><div><img src="https://mc.yandex.ru/watch/21935599" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */
 // <!-- /Yandex.Metrika counter -->
 
 
