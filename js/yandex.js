d5a8631101ce2a14e39b6d5efce2e9e992745d70
GuschinS <guschins@gmail.com>
2021-08-07 12:06:33 +0300

    edit yandex.js

--- js/yandex.js
+++ js/yandex.js
@@ -1,29 +1,48 @@
-		(function(d, w, c) {
-			(w[c] = w[c] || []).push(function() {
-				try {
-					w.yaCounter21935599 = new Ya.Metrika({
-						id: 21935599,
-						clickmap: true,
-						trackLinks: true,
-						accurateTrackBounce: true,
-						webvisor: true,
-						ecommerce: "dataLayer"
-					});
-				} catch (e) {}
-			});
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-			var n = d.getElementsByTagName("script")[0],
-				s = d.createElement("script"),
-				f = function() {
-					n.parentNode.insertBefore(s, n);
-				};
-			s.type = "text/javascript";
-			s.async = true;
-			s.src = "https://mc.yandex.ru/metrika/watch.js";
+   ym(21935599, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        ecommerce:"dataLayer"
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/21935599" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
-			if (w.opera == "[object Opera]") {
-				d.addEventListener("DOMContentLoaded", f, false);
-			} else {
-				f();
-			}
-		})(document, window, "yandex_metrika_callbacks");
+
+
+
+// (function(d, w, c) {
+		// 	(w[c] = w[c] || []).push(function() {
+		// 		try {
+		// 			w.yaCounter21935599 = new Ya.Metrika({
+		// 				id: 21935599,
+		// 				clickmap: true,
+		// 				trackLinks: true,
+		// 				accurateTrackBounce: true,
+		// 				webvisor: true,
+		// 				ecommerce: "dataLayer"
+		// 			});
+		// 		} catch (e) {}
+		// 	});
+
+		// 	var n = d.getElementsByTagName("script")[0],
+		// 		s = d.createElement("script"),
+		// 		f = function() {
+		// 			n.parentNode.insertBefore(s, n);
+		// 		};
+		// 	s.type = "text/javascript";
+		// 	s.async = true;
+		// 	s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+		// 	if (w.opera == "[object Opera]") {
+		// 		d.addEventListener("DOMContentLoaded", f, false);
+		// 	} else {
+		// 		f();
+		// 	}
+		// })(document, window, "yandex_metrika_callbacks");
