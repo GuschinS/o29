2735b2df3aca4444b55671d7386eebd0328ffeb3
GuschinS <guschins@gmail.com>
2021-11-17 14:46:50 +0300

    edit ya metrika

--- js/yandex.js
+++ js/yandex.js
@@ -1,48 +1,11 @@
 // <!-- Yandex.Metrika counter -->
 
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-	m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(21935599, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        ecommerce:"dataLayer"
-   });
-
-/* <noscript><div><img src="https://mc.yandex.ru/watch/21935599" style="position:absolute; left:-9999px;" alt="" /></div></noscript> */
-// <!-- /Yandex.Metrika counter -->
-
-
-
-
-// (function(d, w, c) {
-		// 	(w[c] = w[c] || []).push(function() {
-		// 		try {
-		// 			w.yaCounter21935599 = new Ya.Metrika({
-		// 				id: 21935599,
-		// 				clickmap: true,
-		// 				trackLinks: true,
-		// 				accurateTrackBounce: true,
-		// 				webvisor: true,
-		// 				ecommerce: "dataLayer"
-		// 			});
-		// 		} catch (e) {}
-		// 	});
-
-		// 	var n = d.getElementsByTagName("script")[0],
-		// 		s = d.createElement("script"),
-		// 		f = function() {
-		// 			n.parentNode.insertBefore(s, n);
-		// 		};
-		// 	s.type = "text/javascript";
-		// 	s.async = true;
-		// 	s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-		// 	if (w.opera == "[object Opera]") {
-		// 		d.addEventListener("DOMContentLoaded", f, false);
-		// 	} else {
-		// 		f();
-		// 	}
-		// })(document, window, "yandex_metrika_callbacks");
+ym(86510645, "init", {
+	 clickmap:true,
+	 trackLinks:true,
+	 accurateTrackBounce:true
+});
